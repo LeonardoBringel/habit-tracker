01a53ccbf7cd45e2212b1604263212572beedd62
LeonardoBringel <leo.sbringel@gmail.com>
2022-12-06 10:48:58 -0300

    :tada: Begin project

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HabitTrackerApp());
+}
+
+class HabitTrackerApp extends StatelessWidget {
+  const HabitTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Habit Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Habit Tracker'),
+      ),
+      body: const Center(
+        child: Text('Hello There!', style: TextStyle(fontSize: 32)),
+      ),
+    );
+  }
+}
