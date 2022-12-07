2ebe3dd2a7b02e987935d08d2aa92434bf3a44ba
LeonardoBringel <leo.sbringel@gmail.com>
2022-12-07 02:38:08 -0300

    :recycle: Rename frequency variable to days

--- lib/models/goal.dart
+++ lib/models/goal.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 class Goal {
   final String name;
   final String description;
-  final String frequency;
+  final List<String> days;
   final IconData icon;
   final List<String> completedDates;
 
   const Goal({
     required this.name,
     required this.description,
-    required this.frequency,
+    required this.days,
     required this.icon,
     required this.completedDates,
   });
