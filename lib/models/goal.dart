f6d38f225b7890e780269e9a1d0298fbfde01871
LeonardoBringel <leo.sbringel@gmail.com>
2023-01-04 12:14:42 -0300

    :sparkles: Add id to goals

--- lib/models/goal.dart
+++ lib/models/goal.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Goal {
+  final int id;
   final String name;
   final String description;
   final List<int> days;
@@ -8,6 +9,7 @@ class Goal {
   final List<String> completedDates;
 
   const Goal({
+    this.id = -1,
     required this.name,
     required this.description,
     required this.days,
