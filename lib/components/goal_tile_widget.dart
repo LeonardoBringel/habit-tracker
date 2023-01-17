dbcf226487ed2f2dc4f507edcad7ef659ed9afa6
LeonardoBringel <leo.sbringel@gmail.com>
2023-01-17 15:16:48 -0300

    :recycle: Convert goal tile widget to stateless

--- lib/components/goal_tile_widget.dart
+++ lib/components/goal_tile_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../models/goal.dart';
 
-class GoalTileWidget extends StatefulWidget {
+class GoalTileWidget extends StatelessWidget {
   const GoalTileWidget({
     super.key,
     required this.goal,
@@ -23,31 +23,24 @@ class GoalTileWidget extends StatefulWidget {
   };
 
   @override
-  State<GoalTileWidget> createState() => _GoalTileWidgetState();
-}
-
-class _GoalTileWidgetState extends State<GoalTileWidget> {
-  @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        widget.goal.name,
-        style: widget.isCompleted
+        goal.name,
+        style: isCompleted
             ? const TextStyle(
                 fontSize: 24, decoration: TextDecoration.lineThrough)
             : const TextStyle(fontSize: 24),
         textAlign: TextAlign.center,
       ),
       subtitle: Text(
-        widget.goal.days.contains(false)
-            ? GoalTileWidget.weekdays[DateTime.now().weekday]!
-            : 'Daily',
+        goal.days.contains(false) ? weekdays[DateTime.now().weekday]! : 'Daily',
         style: const TextStyle(fontSize: 18),
         textAlign: TextAlign.center,
       ),
       leading: Icon(
         IconData(
-          widget.goal.iconId,
+          goal.iconId,
           fontFamily: 'MaterialIcons',
         ),
       ),
