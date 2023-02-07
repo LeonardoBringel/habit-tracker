ac14d5e010f590a31d40a9a2b1c76099fad55cfe
LeonardoBringel <leo.sbringel@gmail.com>
2023-02-07 00:13:58 -0300

    :lipstick: Add color theme

---
+++ lib/theme/color_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorTheme {
+  static const Color primary = Color.fromRGBO(255, 235, 59, 1);
+  static const Color secondary = Color.fromRGBO(252, 163, 17, 1);
+  static const Color background = Color.fromRGBO(0, 8, 20, 1);
+
+  static const Color faded = Color.fromRGBO(66, 66, 66, 1);
+  static const Color alert = Color.fromRGBO(140, 0, 0, 1);
+}
