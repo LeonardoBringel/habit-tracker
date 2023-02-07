a50343919e35cf48332a136a91c9f37792969f1d
LeonardoBringel <leo.sbringel@gmail.com>
2023-02-07 01:37:35 -0300

    :adhesive_bandage: Adjust empty goals message widget to prevent keyboard overflow

--- lib/components/empty_goals_message_widget.dart
+++ lib/components/empty_goals_message_widget.dart
@@ -5,27 +5,29 @@ class EmptyGoalsMessageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(40, 120, 40, 60),
-      child: Column(
-        children: [
-          Image.asset(
-            'assets/images/spooky_ghost.png',
-            height: 200,
-            width: 200,
-          ),
-          const SizedBox(height: 50),
-          const Text(
-            'Oooooops!',
-            style: TextStyle(fontSize: 24),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            'This place looks empty, let\'s create some goals before more ghosts arrive!',
-            style: TextStyle(fontSize: 18),
-            textAlign: TextAlign.center,
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 120, 40, 60),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/spooky_ghost.png',
+              height: 200,
+              width: 200,
+            ),
+            const SizedBox(height: 50),
+            const Text(
+              'Oooooops!',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'This place looks empty, let\'s create some goals before more ghosts arrive!',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
