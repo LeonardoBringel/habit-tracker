d5ff27e2e8bc4d237c553d12818eeae3872a7378
LeonardoBringel <leo.sbringel@gmail.com>
2023-02-15 15:19:40 -0300

    :lipstick: Modify snackbar message style

--- lib/components/snackbar_message.dart
+++ lib/components/snackbar_message.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/theme/color_theme.dart';
 
 void snackbarMessage(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      content: Text(message),
+      content: Text(
+        message,
+        style: const TextStyle(
+          fontSize: 18,
+          color: ColorTheme.primary,
+        ),
+      ),
+      backgroundColor: ColorTheme.faded,
       duration: const Duration(seconds: 3),
     ),
   );
